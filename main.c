e9defd3ccfb1521fe33edbaac4a4e18b674f1063
katedayun <58518683+katedayun@users.noreply.github.com>
2023-09-14 16:38:52 +0900

    2

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello, Dayooooooni!\n");
+	printf("Hello, Dayooooni\n");
 	getchar();
 	return 0;
 }
